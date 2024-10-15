9817b5034db5f7daaca32dc82a5c91df6996fe97
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-10-15 17:05:31 +0200

    Fixed the sign in page.

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,105 +1,105 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import PageBanner from "../../components/PageBanner";
-import signup from "../../assets/images/signin.jpg";
-import { useForm } from "react-hook-form";
-import { useUserSignInMutation } from "../../features/auth/authAPI";
-import { toast } from "react-hot-toast";
-import { storeToken } from "../../utils/token";
-import { userPersistencyReducer } from "../../features/auth/authSlice";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import PageBanner from '../../components/PageBanner'
+import signup from '../../assets/images/signin.jpg'
+import { useForm } from 'react-hook-form'
+import { useUserSignInMutation } from '../../features/auth/authAPI'
+import { toast } from 'react-hot-toast'
+import { storeToken } from '../../utils/token'
+import { userPersistencyReducer } from '../../features/auth/authSlice'
+import { useDispatch } from 'react-redux'
 
-export default function SignIn() {
+export default function SignIn () {
   const navigate = useNavigate()
-  const distaptch = useDispatch();
+  const distaptch = useDispatch()
 
   const {
     reset,
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    formState: { errors }
+  } = useForm()
 
   const [signin, { data, isLoading, isSuccess, isError, error }] =
-    useUserSignInMutation();
+    useUserSignInMutation()
 
-  const signinHandle = (useData) => {
-    signin(useData);
-    reset();
-  };
+  const signinHandle = useData => {
+    signin(useData)
+    reset()
+  }
 
   useEffect(() => {
     if (isLoading)
-      toast.loading("Signing Up...", { id: "loading", duration: 800 });
+      toast.loading('Signing Up...', { id: 'loading', duration: 800 })
 
     if (isSuccess) {
-      storeToken(data.data.token);
-      toast.success("Signed In", { id: "succ" });
-      distaptch(userPersistencyReducer(data.data.user));
+      storeToken(data.data.token)
+      toast.success('Signed In', { id: 'succ' })
+      distaptch(userPersistencyReducer(data.data.user))
       navigate('/')
     }
 
-    if (isError) toast.error(error.data.error, { id: "err" });
-  }, [isLoading, isSuccess, isError, data, error, distaptch, navigate]);
+    if (isError) toast.error(error.data.error, { id: 'err' })
+  }, [isLoading, isSuccess, isError, data, error, distaptch, navigate])
 
   return (
     <>
-      <PageBanner bg_img={signup} title="Sign In" />
-      <div className="hero py-8 min-h-[80vh] bg-base-200">
-        <div className="hero-content gap-5 md:gap-14 flex-col lg:flex-row">
-          <div className="text-center flex-1">
-            <h1 className="text-4xl font-bold">Sign In now!</h1>
-            <p className="py-6 max-w-2xl ">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+      <PageBanner bg_img={signup} title='Sign In' />
+      <div className='hero py-8 min-h-[80vh] bg-base-200'>
+        <div className='flex-col gap-5 hero-content md:gap-14 lg:flex-row'>
+          <div className='flex-1 text-center'>
+            <h1 className='text-4xl font-bold'>Sign In now!</h1>
+            <p className='max-w-2xl py-6 '>
+              Welcome to DarVoo! Join us to explore authentic Nigerian flavors,
+              get exclusive updates, and enjoy a seamless catering experience.
+              Sign up now to bring a taste of home to your table!
             </p>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
-            <div className="card-body">
+          <div className='flex-shrink-0 w-full max-w-xl shadow-2xl card bg-base-100'>
+            <div className='card-body'>
               <form onSubmit={handleSubmit(signinHandle)}>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Email</span>
                   </label>
                   <input
-                    type="email"
-                    placeholder="riyadhossain@gmail.com"
-                    className="input rounded-md input-bordered"
-                    {...register("email", { required: true })}
+                    type='email'
+                    placeholder='riyadhossain@gmail.com'
+                    className='rounded-md input input-bordered'
+                    {...register('email', { required: true })}
                   />
                   {errors.email && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Email is required
                     </span>
                   )}
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Password</span>
                   </label>
                   <input
-                    type="password"
-                    placeholder="password"
-                    className="input rounded-md input-bordered"
-                    {...register("password", { required: true })}
+                    type='password'
+                    placeholder='password'
+                    className='rounded-md input input-bordered'
+                    {...register('password', { required: true })}
                   />
                   {errors.password && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Password is required
                     </span>
                   )}
-                  <label className="label">
-                    <span className="label-text-alt">
-                      Don't have any account?{" "}
-                      <Link className=" link link-hover link-info" to="/signup">
+                  <label className='label'>
+                    <span className='label-text-alt'>
+                      Don't have any account?{' '}
+                      <Link className=' link link-hover link-info' to='/signup'>
                         Sign Up
                       </Link>
                     </span>
                   </label>
                 </div>
-                <div className="form-control mt-6 flex flex-col gap-3">
-                  <button type="submit" className="btn">
+                <div className='flex flex-col gap-3 mt-6 form-control'>
+                  <button type='submit' className='btn'>
                     Sign In
                   </button>
                   {/* <button className="btn">Continue With Google</button> */}
@@ -110,5 +110,5 @@ export default function SignIn() {
         </div>
       </div>
     </>
-  );
+  )
 }
