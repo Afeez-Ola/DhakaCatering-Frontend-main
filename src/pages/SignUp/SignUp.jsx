a19b67a68b49df8f7b308e841607d26ac1cd9125
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-10-15 17:03:56 +0200

    fixed the signup page.

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,67 +1,67 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import PageBanner from "../../components/PageBanner";
-import signup from "../../assets/images/signin.jpg";
-import { useForm, useWatch } from "react-hook-form";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useUserSignUpMutation } from "../../features/auth/authAPI";
-import { toast } from "react-hot-toast";
-import { useRef } from "react";
-import { storeToken } from "../../utils/token";
-import { userPersistencyReducer } from "../../features/auth/authSlice";
-import { useDispatch } from "react-redux";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import PageBanner from '../../components/PageBanner'
+import signup from '../../assets/images/signin.jpg'
+import { useForm, useWatch } from 'react-hook-form'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useUserSignUpMutation } from '../../features/auth/authAPI'
+import { toast } from 'react-hot-toast'
+import { useRef } from 'react'
+import { storeToken } from '../../utils/token'
+import { userPersistencyReducer } from '../../features/auth/authSlice'
+import { useDispatch } from 'react-redux'
 
-export default function SignUp() {
-  const navigate = useNavigate();
-  const distaptch = useDispatch();
-  const [disabled, setDisabled] = useState(true);
-  let errorMessage = useRef("Something went wrong");
+export default function SignUp () {
+  const navigate = useNavigate()
+  const distaptch = useDispatch()
+  const [disabled, setDisabled] = useState(true)
+  let errorMessage = useRef('Something went wrong')
 
   const [signupFunc, { isLoading, isError, isSuccess, error, data }] =
-    useUserSignUpMutation();
+    useUserSignUpMutation()
 
   const {
     reset,
     control,
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    formState: { errors }
+  } = useForm()
 
-  const password = useWatch({ control, name: "password" });
-  const confirmPassword = useWatch({ control, name: "confirmPassword" });
+  const password = useWatch({ control, name: 'password' })
+  const confirmPassword = useWatch({ control, name: 'confirmPassword' })
 
   useEffect(() => {
     if (password === confirmPassword && confirmPassword) {
-      setDisabled(false);
+      setDisabled(false)
     } else {
-      setDisabled(true);
+      setDisabled(true)
     }
-  }, [password, confirmPassword]);
+  }, [password, confirmPassword])
 
-  const handleSignUp = async (userData) => {
-    signupFunc(userData);
-    reset();
-  };
+  const handleSignUp = async userData => {
+    signupFunc(userData)
+    reset()
+  }
 
   useEffect(() => {
     if (isLoading)
-      toast.loading("Signing Up...", { id: "loading", duration: 800 });
+      toast.loading('Signing Up...', { id: 'loading', duration: 800 })
 
     if (error) {
-      if (error.data.error.includes("duplicate key error"))
-        errorMessage.current = "User with same credentials is exist";
+      if (error.data.error.includes('duplicate key error'))
+        errorMessage.current = 'User with same credentials is exist'
     }
 
     if (isSuccess) {
-      storeToken(data.data.token);
-      toast.success("Signed In", { id: "succ" });
-      distaptch(userPersistencyReducer(data.data.user));
-      navigate("/");
+      storeToken(data.data.token)
+      toast.success('Signed In', { id: 'succ' })
+      distaptch(userPersistencyReducer(data.data.user))
+      navigate('/')
     }
     if (isError)
-      toast.error(error.data.error || errorMessage.current, { id: "err" });
+      toast.error(error.data.error || errorMessage.current, { id: 'err' })
   }, [
     isLoading,
     isSuccess,
@@ -70,110 +70,110 @@ export default function SignUp() {
     error,
     data,
     distaptch,
-    navigate,
-  ]);
+    navigate
+  ])
 
   return (
     <>
-      <PageBanner bg_img={signup} title="Sign Up" />
-      <div className="hero pt-12 min-h-screen bg-base-200 pb-12">
-        <div className="hero-content gap-5 md:gap-14 flex-col lg:flex-row">
-          <div className="text-center flex-1">
-            <h1 className="text-4xl font-bold">Sign Up Now!</h1>
-            <p className="py-6 max-w-2xl ">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+      <PageBanner bg_img={signup} title='Sign Up' />
+      <div className='min-h-screen pt-12 pb-12 hero bg-base-200'>
+        <div className='flex-col gap-5 hero-content md:gap-14 lg:flex-row'>
+          <div className='flex-1 text-center'>
+            <h1 className='text-4xl font-bold'>Sign Up Now!</h1>
+            <p className='max-w-2xl py-6 '>
+              Welcome to DarVoo! Join us to explore authentic Nigerian flavors,
+              get exclusive updates, and enjoy a seamless catering experience.
+              Sign up now to bring a taste of home to your table!
             </p>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
-            <div className="card-body">
+          <div className='flex-shrink-0 w-full max-w-xl shadow-2xl card bg-base-100'>
+            <div className='card-body'>
               <form
                 onSubmit={handleSubmit(handleSignUp)}
-                className="flex flex-col gap-1"
+                className='flex flex-col gap-1'
               >
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Full Name</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Full Name</span>
                   </label>
                   <input
-                    type="text"
-                    placeholder="Riyad Hossain"
-                    className="input rounded-md input-bordered"
-                    {...register("fullName", { required: true })}
+                    type='text'
+                    placeholder='Riyad Hossain'
+                    className='rounded-md input input-bordered'
+                    {...register('fullName', { required: true })}
                   />
                   {errors.fullName && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Full Name is required
                     </span>
                   )}
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Email</span>
                   </label>
                   <input
-                    type="email"
-                    placeholder="riyadhossain@gmail.com"
-                    className="input rounded-md input-bordered"
-                    {...register("email", { required: true })}
+                    type='email'
+                    placeholder='riyadhossain@gmail.com'
+                    className='rounded-md input input-bordered'
+                    {...register('email', { required: true })}
                   />
                   {errors.email && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Email is required
                     </span>
                   )}
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Phone</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Phone</span>
                   </label>
                   <input
-                    type="number"
-                    placeholder="01703790978"
-                    className="input rounded-md input-bordered"
-                    {...register("contactNumber", {
+                    type='number'
+                    placeholder='01703790978'
+                    className='rounded-md input input-bordered'
+                    {...register('contactNumber', {
                       required: true,
                       maxLength: 11,
-                      minLength: 11,
+                      minLength: 11
                     })}
                   />
                   {errors.phone && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Valid Bangladeshi number is required
                     </span>
                   )}
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Password</span>
                   </label>
                   <input
-                    type="password"
-                    placeholder="password"
-                    className="input rounded-md input-bordered"
-                    {...register("password", { required: true })}
+                    type='password'
+                    placeholder='password'
+                    className='rounded-md input input-bordered'
+                    {...register('password', { required: true })}
                   />
                   {errors.password && (
-                    <span className="text-error text-xs text-left mt-1">
+                    <span className='mt-1 text-xs text-left text-error'>
                       Password is required
                     </span>
                   )}
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Confirm Password</span>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Confirm Password</span>
                   </label>
                   <input
-                    type="password"
-                    placeholder="password"
-                    className="input rounded-md input-bordered"
-                    {...register("confirmPassword", { required: true })}
+                    type='password'
+                    placeholder='password'
+                    className='rounded-md input input-bordered'
+                    {...register('confirmPassword', { required: true })}
                   />
                 </div>
 
-                <div className="form-control mt-6 flex flex-col gap-3">
-                  <button type="submit" disabled={disabled} className="btn">
+                <div className='flex flex-col gap-3 mt-6 form-control'>
+                  <button type='submit' disabled={disabled} className='btn'>
                     Sign Up
                   </button>
                 </div>
@@ -183,7 +183,7 @@ export default function SignUp() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
 /* <div className="form-control">
@@ -194,7 +194,7 @@ export default function SignUp() {
                     rows={3}
                     type="text"
                     placeholder="PA-150, Bashtola Masgid, South Badda, Dhaka - 1224"
-                    className="textarea rounded-md input-bordered resize-none"
+                    className="rounded-md resize-none textarea input-bordered"
                   />
                   <label className="label">
                     <span className="label-text-alt">
